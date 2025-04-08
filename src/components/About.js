fac25ba7514698bb36a8c94f925d447887426040
VNWabule <vicwabule@gmail.com>
2025-04-08 15:50:20 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">About
+    <h2>About Me</h2>
+    <p>My name is {image.name} and I am a web developer.</p>
+    <img src={image} alt="I made this" />
+  </div>
+  );
 }
 
 export default About;
